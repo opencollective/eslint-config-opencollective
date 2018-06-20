35c2389edcd6fd12f6b7b560d0974e8fa70c4ce4
François Hodierne <francois@hodierne.net>
2018-06-20 09:50:58 +0200

    version 1.3.0:
    - add eslint-plugin-node
    - add eslint-plugin-babel
    - add a few more rules, semi, quotes, object-curly-spacing, comma-dangle

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 module.exports = {
-  "extends": [
-    "eslint:recommended",
-    "plugin:import/warnings",
-    "plugin:react/recommended"
+  'extends': [
+    'eslint:recommended',
+    'plugin:node/recommended',
+    'plugin:import/recommended',
+    'plugin:react/recommended',
   ],
-  "parser": "babel-eslint",
-  "env": {
-    "browser": true,
-    "node": true,
-    "es6": true
+  'parser': 'babel-eslint',
+  'env': {
+    'browser': true,
+    'node': true,
+    'es6': true,
   },
-  "rules": {
+  'rules': {
 
     // -------------------------
     // Base file formating rules
@@ -18,10 +19,10 @@ module.exports = {
 
     // disallow trailing whitespace at the end of lines
     // https://eslint.org/docs/rules/no-trailing-spaces
-    "no-trailing-spaces": 1,
+    'no-trailing-spaces': 1,
     // require newline at the end of files
     // https://eslint.org/docs/rules/eol-last
-    "eol-last": 1,
+    'eol-last': 1,
 
     // ----------------------------------
     // Adding more eslint rules as errors
@@ -29,34 +30,51 @@ module.exports = {
 
     // Disallow Unused Variables
     // https://eslint.org/docs/rules/no-unused-vars
-    "no-unused-vars": [2, { "vars": "all" }],
+    'no-unused-vars': [2, { 'vars': 'all' }],
     // require space before/after arrow function's arrow
     // https://github.com/eslint/eslint/blob/master/docs/rules/arrow-spacing.md
-    "arrow-spacing": [2, { "before": true, "after": true }],
+    'arrow-spacing': [2, { 'before': true, 'after': true }],
     // require using arrow functions as callbacks
     // https://eslint.org/docs/rules/prefer-arrow-callback
-    "prefer-arrow-callback": 2,
+    'prefer-arrow-callback': 2,
     // require using template literals instead of string concatenation
     // http://eslint.org/docs/rules/prefer-template
-    "prefer-template": 2,
+    'prefer-template': 2,
     // require using of const declaration for variables that are never modified after declared
     // https://eslint.org/docs/rules/prefer-const
-    "prefer-const": 2,
+    'prefer-const': 2,
     // disallow modifying variables that are declared using const
     // https://eslint.org/docs/rules/no-const-assign
-    "no-const-assign": 2,
+    'no-const-assign': 2,
     // disallow multiple empty lines (max 2 lines)
     // https://eslint.org/docs/rules/no-multiple-empty-lines
-    "no-multiple-empty-lines": [2, { "max": 2 }],
+    'no-multiple-empty-lines': [2, { 'max': 2 }],
     // one true brace style
     // http://eslint.org/docs/rules/brace-style
-    "brace-style": [2, "1tbs"],
+    'brace-style': [2, '1tbs'],
     // enforce consistent spacing before and after keywords
     // https://eslint.org/docs/rules/keyword-spacing
-    "keyword-spacing": [2, { "before": true, "after": true }],
+    'keyword-spacing': [2, { 'before': true, 'after': true }],
     // require let or const instead of var
     // https://eslint.org/docs/rules/no-var
-    "no-var": 2,
+    'no-var': 2,
+
+    // ------------------------------------
+    // Adding more eslint rules as warnings
+    // ------------------------------------
+
+    // require semicolons instead of ASI
+    // https://eslint.org/docs/rules/semi
+    'babel/semi': [1, 'always'],
+    // enforce the consistent use of single quotes
+    // https://eslint.org/docs/rules/quotes
+    'babel/quotes': [1, 'single'],
+    // enforce consistent spacing inside braces
+    // https://eslint.org/docs/rules/object-curly-spacing
+    'babel/object-curly-spacing': [1, 'always'],
+    // require trailing commas
+    // https://eslint.org/docs/rules/comma-dangle
+    'comma-dangle': [1, 'always-multiline'],
 
     // -------------------------------------------------------------
     // Overhiding react/recommended to (temporarily) relax the rules
@@ -64,16 +82,16 @@ module.exports = {
 
     // prevent missing props validation in a React component definition
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/prop-types.md
-    "react/prop-types": 1,
+    'react/prop-types': 1,
     // detect missing 'key' prop
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-key.md
-    "react/jsx-key": 1,
+    'react/jsx-key': 1,
     // prevent missing React when using JSX
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/react-in-jsx-scope.md
-    "react/react-in-jsx-scope": 1,
+    'react/react-in-jsx-scope': 1,
     // prevent direct mutation of this.state
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-direct-mutation-state.md
-    "react/no-direct-mutation-state": 1,
+    'react/no-direct-mutation-state': 1,
 
     // ---------------------------------
     // Adding more react rules as errors
@@ -81,7 +99,7 @@ module.exports = {
 
     // prevent missing parentheses around multiline JSX
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-wrap-multilines.md
-    "react/jsx-wrap-multilines": 2,
+    'react/jsx-wrap-multilines': 2,
 
     // -----------------------------------
     // Adding more react rules as warnings
@@ -89,51 +107,52 @@ module.exports = {
 
     // enforce the consistent use of double quotes in JSX attributes
     // https://eslint.org/docs/rules/jsx-quotes
-    "jsx-quotes": [1, "prefer-double"],
+    'jsx-quotes': [1, 'prefer-double'],
     // enforce component methods order
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/sort-comp.md
-    "react/sort-comp": 1,
+    'react/sort-comp': 1,
     // validate JSX indentation (2 spaces)
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-indent.md
-    "react/jsx-indent": [1, 2],
+    'react/jsx-indent': [1, 2],
     // validate props indentation in JSX (2 spaces)
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-indent-props.md
-    "react/jsx-indent-props": [1, 2],
+    'react/jsx-indent-props': [1, 2],
     // validate whitespace in and around the JSX opening and closing brackets
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-tag-spacing.md
-    "react/jsx-tag-spacing": 1,
+    'react/jsx-tag-spacing': 1,
     // validate closing bracket location in JSX
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-closing-bracket-location.md
-    "react/jsx-closing-bracket-location": [1, "props-aligned"],
+    'react/jsx-closing-bracket-location': [1, 'props-aligned'],
     // validate closing tag location in JSX
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-closing-tag-location.md
-    "react/jsx-closing-tag-location": 1,
+    'react/jsx-closing-tag-location': 1,
     // disallow spaces inside of curly braces in JSX attributes and expressions
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-curly-spacing.md
-    "react/jsx-curly-spacing": 1,
+    'react/jsx-curly-spacing': 1,
     // disallow spaces around equal signs in JSX attributes
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-equals-spacing.md
-    "react/jsx-equals-spacing": 1,
+    'react/jsx-equals-spacing': 1,
     // configure the position of the first property
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-first-prop-new-line.md
-    "react/jsx-first-prop-new-line": 1
-
+    'react/jsx-first-prop-new-line': 1,
   },
-  "plugins": [
-    "import",
-    "react"
+  'plugins': [
+    'babel',
+    'import',
+    'node',
+    'react',
   ],
-  "settings": {
-    "react": {
-      "pragma": "React",
-      "version": "16.3.2"
-    }
+  'settings': {
+    'react': {
+      'pragma': 'React',
+      'version': '16.4.1',
+    },
   },
-  "parserOptions": {
-    "ecmaVersion": 6,
-    "sourceType": "module",
-    "ecmaFeatures": {
-      "jsx": true
-    }
-  }
+  'parserOptions': {
+    'ecmaVersion': 6,
+    'sourceType': 'module',
+    'ecmaFeatures': {
+      'jsx': true,
+    },
+  },
 };
